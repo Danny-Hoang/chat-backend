fdf32287281ccd197c845ecfca09615c24573d9d
palteksolution <palteksolution@gmail.com>
2022-10-19 17:46:36 +0700

    update

---
+++ routes/chat.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+// database
+let pool = require("../config/mysql");
+
+router.get("/recent", async function (req, res) {
+    let sql = `SELECT e.id, e.title, e.start_time, e.end_time FROM Event e
+        WHERE 1
+        ORDER BY e.start_time
+        LIMIT 10 
+    `;
+	let [results] = await pool.query(sql, [event_id]);
+	res.send({ error: false, data: results, message: "chat room list." });
+});
