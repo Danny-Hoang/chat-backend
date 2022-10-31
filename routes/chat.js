d11956d9187a109128bbe27a8c72946a106e2388
tqtrung <tqtrung@humaxdigital.com>
2022-10-31 19:06:11 +0700

    update get room message

--- routes/chat.js
+++ routes/chat.js
@@ -16,19 +16,15 @@ router.get("/recent", async function (req, res) {
 // route temp
 router.get("/:userId", async function (req, res) {
     const userId = req.params.userId
-    let sql = `SELECT d.id, d.title, u.username, c2.event_id, c2.content, c2.maxtime FROM Event d LEFT JOIN 
+    let sql = `SELECT d.id, d.title, u.username, c2.event_id, c2.content, c2.time FROM Event d LEFT JOIN 
     (
-                SELECT user_id,
-                       content,
-                       event_id,
-                       MAX(time) AS maxtime
-                FROM Message
-                GROUP BY event_id
+                SELECT m.* FROM Message m WHERE m.id = (SELECT id FROM Message WHERE m.event_id = Message.event_id ORDER BY time DESC LIMIT 1)
+                
             ) c2
                 ON c2.event_id = d.id
         LEFT JOIN User u
             ON c2.user_id = u.id
-    WHERE ? = ?
+    WHERE ? = ? ORDER BY c2.time DESC
     `;
 	let [results] = await pool.query(sql, [userId, userId]);
 	res.send({ error: false, data: results, message: "chat room list." });
